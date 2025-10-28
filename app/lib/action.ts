899d215c8cc92b908ed46b389bb1c398635c4281
volodymyrmalenko <volodymyrmalenko9@gamil.com>
2025-10-28 22:20:19 +0300

    Revalidate and redirect

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,6 +1,10 @@
 'use server';
 
 import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require'});
 
 const FormSchema = z.object({
     id: z.string(),
@@ -20,4 +24,11 @@ export async function createInvoice(formData: FormData){
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+        INSERT INTO invoices ( customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices');
 }
